f9ac96426a5a9fb7089c3f56fe6529a2a6f5497d
amul-agrawal <amul.agrawal@students.iiit.ac.in>
2022-09-24 16:57:54 +0530

    updated DSU

--- src/content/data-structures/Dsu.h
+++ src/content/data-structures/Dsu.h
@@ -12,15 +12,18 @@ struct DSU {
 	DSU(int n) : p(n, -1), s(n, 1), sets(n) {}
 	bool IsSameSet(int a, int b) { return find(a) == find(b); }
 	int find(int x) {return p[x] == -1 ? x : p[x] = find(p[x]);}
-	void join(int a, int b) {
+	bool join(int a, int b) {
 		a = find(a), b = find(b);
-		if (a == b) return;
+		if (a == b) return false;
 		if (s[a] < s[b]) swap(a, b);
 		ss.push({a, s[a]}); sp.push({b, p[b]});
-		sets--; s[a] += s[b]; p[b] = a;
+		sets--; s[a] += s[b]; p[b] = a; return true;
 	}
-	void rollback() {
-		p[sp.top().first] = sp.top().second; sp.pop();
-		s[ss.top().first] = ss.top().second; ss.pop();
+	int time() {return sz(ss);}
+	void rollback(int t) {
+		while(time() > t) {
+			p[sp.top().first] = sp.top().second; sp.pop();
+			s[ss.top().first] = ss.top().second; ss.pop();
+		}
 	}
 };
